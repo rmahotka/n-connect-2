0ce96c1414cd2488492b404434903e172a52568b
rmahotka <rmahotka@gmail.com>
2024-06-06 23:09:54 +0300

    edit component

--- src/components/ContactItemComponent.vue
+++ src/components/ContactItemComponent.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="single-contact-info">
-    <div class="row">
-      <div class="col-5">
-        <img :src="contact.image" alt="contact" />
-      </div>
-      <div class="col-7">
-        <h6>{{ contact.name }}</h6>
-        <span class="p-like">{{ contact.role }}</span>
-        <ul>
-          <li>{{ contact.phone }}</li>
-          <li>{{ contact.email }}</li>
-        </ul>
-      </div>
+    <div>
+      <img :src="contact.image" alt="contact" />
+    </div>
+    <div class="col-7">
+      <h6>{{ contact.name }}</h6>
+      <span class="p-like">{{ contact.role }}</span>
+      <ul>
+        <li>{{ contact.phone }}</li>
+        <li>{{ contact.email }}</li>
+      </ul>
     </div>
   </div>
 </template>
