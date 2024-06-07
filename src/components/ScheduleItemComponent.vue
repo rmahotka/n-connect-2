24634d7990279d4d78276d4a528ded332d3cc23b
mahotich <rmahotka@gmail.com>
2024-06-07 11:54:35 +0300

    edit schedule

--- src/components/ScheduleItemComponent.vue
+++ src/components/ScheduleItemComponent.vue
@@ -1,46 +1,43 @@
 <template>
-  <div class="schedule-card">
-    <div class="collapse show">
-      <div class="card-body">
-        <div class="row">
-          <div class="col-sm-4">
-            <div class="schedule-img">
-              <img :src="schedule.image" alt="schedule" />
-            </div>
-          </div>
-          <div class="col-sm-8">
-            <div class="schedule-info">
-              <div class="schedule-content">
-                <p>{{ schedule.description }}</p>
-                <h5>
-                  <span>With</span> : {{ schedule.speaker.name }}
-                  <small
-                    >{{ schedule.speaker.role }} at
-                    {{ schedule.speaker.company }}</small
-                  >
-                </h5>
-              </div>
-              <div class="schedule-button">
-                <button role="button">
-                  <span class="text">Register</span>
-                </button>
-              </div>
-            </div>
-          </div>
+  <div class="grid lg:grid-cols-2 grid-cols-1 justify-items-center">
+    <div class="schedule-img w-3/4">
+      <img :src="schedule.image" alt="schedule" class="rounded-xl" />
+    </div>
+    <div
+      class="schedule-info h-full flex justify-center gap-12 items-center flex-col"
+    >
+      <div class="">
+        <div>
+          <p class="text-white w-full text-center lg:text-left">
+            {{ schedule.description }}
+          </p>
+          <h5 class="text-xl font-bold text-center lg:text-left">
+            <span class="uppercase text-2xl">With</span> :
+            {{ schedule.speaker.name }}
+            <small class="lg:ml-7"
+              >{{ schedule.speaker.role }} at
+              {{ schedule.speaker.company }}</small
+            >
+          </h5>
         </div>
+      </div>
+      <div class="flex w-full lg:justify-end justify-center">
+        <button
+          role="button"
+          class="bg-white text-black py-2 px-8 rounded-xl font-bold border-0 transition-all border-white hover:bg-black hover:text-white"
+        >
+          Register
+        </button>
       </div>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: "ScheduleItemComponent",
-  props: {
-    schedule: {
-      type: Object,
-      required: true,
-    },
+<script setup>
+defineProps({
+  schedule: {
+    type: Object,
+    required: true,
   },
-};
+});
 </script>
--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -1,12 +1,32 @@
 <template>
   <div class="my-16">
     <div class="container mx-auto">
-      <div class="schedules">
+      <h5 class="font-bold text-3xl text-purple-800 lg:text-left text-center">
+        REGISTER FOR SESSION
+      </h5>
+      <div class="w-full pt-8">
+        <div class="mx-auto w-full rounded-xl bg-[#9476E6] p-2">
+          <Disclosure v-for="(group, time) in groupedSchedules" :key="time">
+            <DisclosureButton
+              class="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-3 text-left text-lg font-bold text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75 mb-2"
+            >
+              <span>{{ time }}</span>
+              <ChevronUpIcon class="h-5 w-5 text-purple-500" />
+            </DisclosureButton>
+            <DisclosurePanel
+              v-for="schedule in group"
+              :key="schedule.id"
+              class="px-4 pb-2 pt-4 text-md text-white"
+            >
+              <ScheduleItemComponent :schedule="schedule" />
+            </DisclosurePanel>
+          </Disclosure>
+        </div>
+      </div>
+
+      <!-- <div class="schedules">
         <div class="flex">
           <div class="schedule-details">
-            <div class="schedule-title">
-              <h5 class="font-bold">REGISTER FOR SESSION</h5>
-            </div>
             <div class="single-schedule card">
               <div
                 class="tab-pane fade show active"
@@ -39,37 +59,28 @@
             </div>
           </div>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
 
-<script>
-import ScheduleItemComponent from '../components/ScheduleItemComponent.vue';
-import schedulesData from '../assets/schedules.json';
+<script setup>
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue';
+import ScheduleItemComponent from '@/components/ScheduleItemComponent.vue';
+import schedulesData from '@/assets/schedules.json';
+import { ref, computed } from 'vue';
 
-export default {
-  name: 'ScheduleAreaComponent',
-  components: {
-    ScheduleItemComponent,
-  },
-  data() {
-    return {
-      schedules: schedulesData,
-    };
-  },
-  computed: {
-    groupedSchedules() {
-      return this.schedules.reduce((acc, schedule) => {
-        if (!acc[schedule.time]) {
-          acc[schedule.time] = [];
-        }
-        acc[schedule.time].push(schedule);
-        return acc;
-      }, {});
-    },
-  },
-};
+const schedules = ref(schedulesData);
+
+const groupedSchedules = computed(() => {
+  return schedules.value.reduce((acc, schedule) => {
+    if (!acc[schedule.time]) {
+      acc[schedule.time] = [];
+    }
+    acc[schedule.time].push(schedule);
+    return acc;
+  }, {});
+});
 </script>
 
 <style scoped></style>
